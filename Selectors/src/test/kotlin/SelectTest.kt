416227e3b514af43f8aecb0204d261c70e3e1bfb
wave1008 <wave1008@gmail.com>
2022-10-14 03:41:40 +0900

    SelectorTest

---
+++ Selectors/src/test/kotlin/SelectTest.kt
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import shirates.core.driver.commandextension.*
+import shirates.core.testcode.UITest
+
+class SelectTest : UITest() {
+
+    @Test
+    @Order(10)
+    fun selectByText() {
+
+        scenario {
+            case(1) {
+                action {
+                    it.select("Network & internet")
+                }.expectation {
+                    it.textIs("Network & internet")
+                }
+            }
+            case(2) {
+                action {
+                    it.selectWithScrollDown("System")
+                }.expectation {
+                    it.textIs("System")
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(20)
+    fun selectByAccessibility() {
+
+        scenario {
+            case(1) {
+                condition {
+                    it.tapWithScrollDown("System")
+                }.action {
+                    it.select("@Navigate up")
+                }.expectation {
+                    it.accessIs("Navigate up")
+                }
+            }
+        }
+    }
+
+}
