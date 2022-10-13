e50ff6ead7dd9ef7bac84c48b8360447d114afa4
wave1008 <wave1008@gmail.com>
2022-10-14 00:09:32 +0900

    SelectorTest

---
+++ Selectors_ja/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Selectors_ja"
+
---
+++ Selectors_ja/src/test/kotlin/SelectTest.kt
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import shirates.core.driver.commandextension.*
+import shirates.core.testcode.UITest
+
+class SelectTest : UITest() {
+
+    @Test
+    @Order(10)
+    @DisplayName("テキスト属性で要素を取得する")
+    fun selectByText() {
+
+        scenario {
+            case(1) {
+                action {
+                    it.select("ネットワークとインターネット")
+                }.expectation {
+                    it.textIs("ネットワークとインターネット")
+                }
+            }
+            case(2) {
+                action {
+                    it.selectWithScrollDown("システム")
+                }.expectation {
+                    it.textIs("システム")
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(20)
+    @DisplayName("アクセシビリティ属性(content-desc)で要素を取得する")
+    fun selectByAccessibility() {
+
+        scenario {
+            case(1) {
+                condition {
+                    it.tapWithScrollDown("システム")
+                }.action {
+                    it.select("@上へ移動")
+                }.expectation {
+                    it.accessIs("上へ移動")
+                }
+            }
+        }
+    }
+
+}
