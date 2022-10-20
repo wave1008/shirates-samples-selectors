d11a6ad967aff7c05aa863c2271977ca61af6001
wave1008 <wave1008@gmail.com>
2022-10-21 06:53:38 +0900

    SelectWithXmlTest

---
+++ Selectors/src/test/kotlin/SelectWithXmlTest.kt
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import shirates.core.driver.commandextension.classIs
+import shirates.core.driver.commandextension.restartApp
+import shirates.core.driver.commandextension.select
+import shirates.core.testcode.UITest
+
+class SelectWithXmlTest : UITest() {
+
+    @Test
+    @Order(10)
+    fun selectWithXml() {
+
+        scenario {
+            case(1) {
+                condition {
+                    it.restartApp()
+                }.expectation {
+                    it.select("Network & internet") {
+                        select(":next").classIs("android.widget.TextView")
+                        select(":next(2)").classIs("android.widget.LinearLayout")
+                        select(":next(3)").classIs("android.widget.LinearLayout")
+                        select(":next(4)").classIs("android.widget.ImageView")
+                        select(":next(5)").classIs("android.widget.RelativeLayout")
+                        select(":next(6)").classIs("android.widget.TextView")
+                        select(":next(7)").classIs("android.widget.TextView")
+                    }
+                }
+            }
+        }
+    }
+
+}
